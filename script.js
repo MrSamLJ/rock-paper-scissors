e583537859f446e9c1620348ee3752fafdbae320
Sam <samuel.l.jefferies@gmail.com>
2021-12-17 17:10:03 +0000

    Adding first touches

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+alertFunction = function() {
+   alert("Hello!");
+};
+
+
+//Add event listener to images
+const image = document.querySelectorAll('img');
+image.addEventListener('click', alertFunction);
 
 let computerSelection = "null";
 
@@ -18,13 +26,19 @@ computerSelection = 'scissors';
 }
 }
 
+//Firing here as game function commented out.
+computerPlay();
+
 //Prompt user for their choice
-function playerChoose() {
-let promptSelection = prompt("Please enter your choice of: Rock, Paper or Scissors", "type here");
-playerSelection = promptSelection.toLowerCase();
-console.log("The player chooses " + playerSelection);
-return playerSelection;
-}
+// function playerChoose() {
+// let promptSelection = prompt("Please enter your choice of: Rock, Paper or Scissors", "type here");
+// playerSelection = promptSelection.toLowerCase();
+// console.log("The player chooses " + playerSelection);
+// return playerSelection;
+// }
+
+//Firing here as game function commented out.
+// playerChoose();
 
 //Count wins
 let totalUserWins = 0;
@@ -35,7 +49,7 @@ let roundCount = 0;
 function playRound() {
 if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'scissors')) {
 console.log("Computer wins!"); 
-++totalComputerWins;
+// ++totalComputerWins;
 return; 
 } else if 
 (playerSelection === computerSelection) {
@@ -43,32 +57,34 @@ console.log("Draw!!");
 return;
 } else {
 console.log("User wins!");
-++totalUserWins;
+// ++totalUserWins;
 return;
 }
 }
 
-   //Play game 5 times
-function game() {
-   for (let i = 0; i < 5; i++) {
-   console.log(1);
-     const playerSelection = playerChoose();
-     const computerSelection = computerPlay();
-     // Call playRound function, passing in newly returned values
-     // from the playerPlay and computerPlay functions
-     const currentRound = playRound(playerSelection, computerSelection);
-     // Log our result
-     console.log("User win count: " + totalUserWins);
-     console.log("Computer win count: " + totalComputerWins);
-   }
-}
+// playRound();
+
+//    //Play game 5 times
+// function game() {
+//    for (let i = 0; i < 5; i++) {
+//    console.log(1);
+//      const playerSelection = playerChoose();
+//      const computerSelection = computerPlay();
+//      // Call playRound function, passing in newly returned values
+//      // from the playerPlay and computerPlay functions
+//      const currentRound = playRound(playerSelection, computerSelection);
+//      // Log our result
+//      console.log("User win count: " + totalUserWins);
+//      console.log("Computer win count: " + totalComputerWins);
+//    }
+// }
 
 //Run game loop
-game();
+// game();
 
-//Give final score
-if (totalUserWins > totalComputerWins) {
-console.log("The winner is the user!!!");
-} else {
-console.log("The winner is the computer!!");
-};
+// //Give final score
+// if (totalUserWins > totalComputerWins) {
+// console.log("The winner is the user!!!");
+// } else {
+// console.log("The winner is the computer!!");
+// };
